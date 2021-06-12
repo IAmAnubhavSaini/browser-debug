5bb44b51f00c0c0f8891beba8082f83f2dcb3fc9
Anubhav <iamanubhavsaini+git@gmail.com>
2021-06-12 19:20:28 +0530

    resize event listener

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const main = document.querySelector('main');
 
 function exec() {
+    main.innerText = '';
     Object.keys(window).filter(k => typeof window[k] === 'string' || typeof window[k] === 'number')
         .map(k => ({k, v: window[k]}))
         .map(({k, v}) => {
             const d = document.createElement('div');
             d.innerText = `${k}: ${v}`;
             main.appendChild(d);
-
         });
-
-
 }
+
+
+window.addEventListener('resize', exec);
