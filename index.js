b4ed9d032c37cdd923d00e6811d4dec2ace81acb
Anubhav Saini <iamanubhavsaini+git@gmail.com>
2024-02-26 23:59:52 +0530

    cleanup

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function exec() {
 
 function printMouse(e) {
     mouse_text.innerText = '';
-    for(const x in e) {
+    for (const x in e) {
         const keys = ['altKey', 'ctrlKey', 'metaKey', 'shiftKey', 'x', 'y'];
-        if(keys.includes(x)) {
+        if (keys.includes(x)) {
             const d = document.createElement('div');
             d.classList.add('item', 'mouse');
             d.innerText = `${x}: ${e[x]}`;
@@ -26,62 +26,53 @@ function printMouse(e) {
     }
 }
 
+function keydownHandler(event) {
+    // Prevent repeating actions if key is held down
+    if (event.repeat) return;
+
+
+    switch (event.key) {
+        case "Shift":
+            document.querySelector('.key[data-key="SHIFT"]').classList.add('pressed');
+            break;
+        case "Control":
+            document.querySelector('.key[data-key="CONTROL"]').classList.add('pressed');
+            break;
+        case "Alt":
+            document.querySelector('.key[data-key="ALT"]').classList.add('pressed');
+            break;
+        case "Meta":
+            document.querySelector('.key[data-key="META"]').classList.add('pressed');
+            break;
+        case "Backspace":
+            document.querySelector('.key[data-key="BACKSPACE"]').classList.add('pressed');
+            break;
+        case "Enter":
+            document.querySelector('.key[data-key="ENTER"]').classList.add('pressed');
+            break;
+        case " ":
+            document.querySelector('.key[data-key="SPACE"]').classList.add('pressed');
+            break;
+        case "Escape":
+            document.querySelector('.key[data-key="ESCAPE"]').classList.add('pressed');
+            break;
+        default:
+            const keyElement = document.querySelector(`.key[data-key="${event.key.toUpperCase()}"]`);
+            if (keyElement) keyElement.classList.add('pressed');
+            return;
+    }
+
+}
+
+function keyupHandler(event) {
+    const pressedKey = document.querySelector('.key.pressed');
+    if (pressedKey) pressedKey.classList.remove('pressed');
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    const special = [
-        'Enter',
-        'Space',
-        'Backspace',
-        'Shift',
-        'Ctrl',
-        'Alt',
-        'Meta',
-    ];
-
-    // Listen for keydown events
-    document.addEventListener('keydown', function(event) {
-        // Prevent repeating actions if key is held down
-        if (event.repeat) return;
-
-
-        switch (event.key) {
-            case "Shift":
-                document.querySelector('.key[data-key="SHIFT"]').classList.add('pressed');
-                break;
-            case "Control":
-                document.querySelector('.key[data-key="CONTROL"]').classList.add('pressed');
-                break;
-            case "Alt":
-                document.querySelector('.key[data-key="ALT"]').classList.add('pressed');
-                break;
-            case "Meta":
-                document.querySelector('.key[data-key="META"]').classList.add('pressed');
-                break;
-            case "Backspace":
-                document.querySelector('.key[data-key="BACKSPACE"]').classList.add('pressed');
-                break;
-            case "Enter":
-                document.querySelector('.key[data-key="ENTER"]').classList.add('pressed');
-                break;
-            case " ":
-                document.querySelector('.key[data-key="SPACE"]').classList.add('pressed');
-                break;
-            case "Escape":
-                document.querySelector('.key[data-key="ESCAPE"]').classList.add('pressed');
-                break;
-            default:
-                const keyElement = document.querySelector(`.key[data-key="${event.key.toUpperCase()}"]`);
-                if (keyElement) keyElement.classList.add('pressed');
-                return; // Quit when this doesn't handle the key event.
-        }
-
-    });
-
-    // Listen for keyup events
-    document.addEventListener('keyup', function(event) {
-        document.querySelector('.key.pressed').classList.remove('pressed');
-    });
+    document.addEventListener('keydown', keydownHandler);
+    document.addEventListener('keyup', keyupHandler);
 });
-
 
 window.addEventListener('resize', exec);
 window.addEventListener('mousemove', printMouse);
