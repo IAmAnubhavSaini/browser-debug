8a7d10e49b0b61701994d372aa2c7eef87766c1f
Anubhav Saini <iamanubhavsaini+git@gmail.com>
2024-02-26 23:54:43 +0530

    add keyboard layout

--- index.js
+++ index.js
@@ -26,6 +26,63 @@ function printMouse(e) {
     }
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    const special = [
+        'Enter',
+        'Space',
+        'Backspace',
+        'Shift',
+        'Ctrl',
+        'Alt',
+        'Meta',
+    ];
+
+    // Listen for keydown events
+    document.addEventListener('keydown', function(event) {
+        // Prevent repeating actions if key is held down
+        if (event.repeat) return;
+
+
+        switch (event.key) {
+            case "Shift":
+                document.querySelector('.key[data-key="SHIFT"]').classList.add('pressed');
+                break;
+            case "Control":
+                document.querySelector('.key[data-key="CONTROL"]').classList.add('pressed');
+                break;
+            case "Alt":
+                document.querySelector('.key[data-key="ALT"]').classList.add('pressed');
+                break;
+            case "Meta":
+                document.querySelector('.key[data-key="META"]').classList.add('pressed');
+                break;
+            case "Backspace":
+                document.querySelector('.key[data-key="BACKSPACE"]').classList.add('pressed');
+                break;
+            case "Enter":
+                document.querySelector('.key[data-key="ENTER"]').classList.add('pressed');
+                break;
+            case " ":
+                document.querySelector('.key[data-key="SPACE"]').classList.add('pressed');
+                break;
+            case "Escape":
+                document.querySelector('.key[data-key="ESCAPE"]').classList.add('pressed');
+                break;
+            default:
+                const keyElement = document.querySelector(`.key[data-key="${event.key.toUpperCase()}"]`);
+                if (keyElement) keyElement.classList.add('pressed');
+                return; // Quit when this doesn't handle the key event.
+        }
+
+    });
+
+    // Listen for keyup events
+    document.addEventListener('keyup', function(event) {
+        document.querySelector('.key.pressed').classList.remove('pressed');
+    });
+});
+
+
 window.addEventListener('resize', exec);
 window.addEventListener('mousemove', printMouse);
 
